2ab1bab64204f10d4b3130cf5e0700abc791c052
sebin vincent <hellosebinvincent@gmail.com>
2020-02-15 17:20:18 +0530

    imports optimized

--- src/main/java/com/example/springauth/config/OAuth2Config.java
+++ src/main/java/com/example/springauth/config/OAuth2Config.java
@@ -8,13 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
--- src/main/java/com/example/springauth/utility/CustomTokenEnhancer.java
+++ src/main/java/com/example/springauth/utility/CustomTokenEnhancer.java
@@ -1,7 +1,6 @@
 package com.example.springauth.utility;
 
 import com.example.springauth.model.User;
-import org.springframework.security.jwt.crypto.sign.SignatureVerifier;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
